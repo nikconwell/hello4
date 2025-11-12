bb6d3f2074c0f782bcaef8a4034262c00d19f2c8
Nik Conwell <nikconwell@gmail.com>
2025-11-12 06:58:32 -0500

    Simple hello world server

---
+++ server.py
@@ -0,0 +1,23 @@
+import http.server
+import ssl
+import socket
+from http.server import BaseHTTPRequestHandler
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        message = f"Hello World from {socket.gethostname()}"
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+PORT = 443
+httpd = http.server.HTTPServer(("", PORT), HelloHandler)
+
+# SSL context
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile="/cert.crt", keyfile="/cert.key")
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+print(f"Serving HTTPS Hello World from {socket.gethostname()} on port {PORT}")
+httpd.serve_forever()
